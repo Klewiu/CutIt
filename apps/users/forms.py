3b481ab67b4baaaae1dba98dd06748a275456848
TomPrint <tomekklewicki@gmail.com>
2022-03-06 02:31:26 +0100

    Updated Login Page

--- apps/users/forms.py
+++ apps/users/forms.py
@@ -8,24 +8,6 @@ class CustomUserCreationForm (UserCreationForm):
         fields = "__all__"
 
 
-
-class LoginForm(forms.Form):
-    username = forms.CharField(
-        widget= forms.TextInput(
-            attrs={
-                "class": "form-control"
-            }
-        )
-    )
-    password = forms.CharField(
-        widget=forms.PasswordInput(
-            attrs={
-                "class": "form-control"
-            }
-        )
-    )
-
-
 class SignUpForm(UserCreationForm):
     username = forms.CharField(
         widget=forms.TextInput(
--- apps/users/urls.py
+++ apps/users/urls.py
@@ -3,12 +3,15 @@ from . import views
 from cutIt_app import settings
 from django.conf.urls.static import static
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path("users_list/", views.users, name="page-users-list"),
-    path("login/", views.login_view, name="page-login"),
+    path('login/', auth_views.LoginView.as_view(template_name = 'users/login.html'), name= 'login'),
+    path('logout/', auth_views.LogoutView.as_view(template_name = 'users/logout.html'), name= 'logout'),
     path("adminpage/register/", views.register, name="page-register"),
-    path("adminagep/", views.admin_page, name="page-admin"),
+    path("adminpage/", views.admin_page, name="page-admin"),
+
 ]
 
 if settings.DEBUG:
