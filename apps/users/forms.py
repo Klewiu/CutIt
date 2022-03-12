cb6c75362323d3fa42a3adf84d29ed42c4dfacb3
TomPrint <tomekklewicki@gmail.com>
2022-03-12 01:37:24 +0100

    couple updates in user management

--- apps/users/forms.py
+++ apps/users/forms.py
@@ -1,3 +1,4 @@
+from tabnanny import verbose
 from django import forms
 from django.contrib.auth.forms import UserCreationForm
 from .models import User
--- apps/users/views.py
+++ apps/users/views.py
@@ -4,13 +4,14 @@ from django.contrib.auth import authenticate, login
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
 from .decorators import admin_required
+from .models import User
 
 # USERS VIEW#
-def users(request):
-    context = {
-        "title": "Lista użytkowników",
-    }
-    return render(request, "users/users_list.html", context)
+# def users(request):
+#     context = {
+#         "title": "Lista użytkowników",
+#     }
+#     return render(request, "users/users_list.html", context)
 
 
 
@@ -21,19 +22,29 @@ def register(request):
     if request.method == "POST":
         form = SignUpForm(request.POST)
         if form.is_valid():
-            user = form.save()
-            msg = "User Account Has Been Created"
-            return redirect("page-register")
-        else:
-            msg = "Form Not Valid"
+            form.save()
+            username = form.cleaned_data.get('username')
+            messages.success(request, f'Utworzono konto dla: {username}')
+            return redirect ('page-admin') #redirect affter successful registration
+
     else:
         form = SignUpForm()
-    return render(request, "users/register.html", {"form": form, "msg": msg})
+    return render(request, 'users/register.html', {'form': form})
 
 @login_required
 @admin_required
 def admin_page(request):
+    users = User.objects.all()
     context = {
+        
         "title": "Admin Page",
+        "users": users,
+        
     }
     return render(request, "users/adminpage.html", context)
+
+
+
+    
+
+    
