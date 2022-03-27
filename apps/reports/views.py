6834e52087e8a0d4a88a532619e00ca509b59e3e
Jakub Klewicki <kuba.klewicki@gmail.com>
2022-03-27 13:49:59 +0200

    Changes

--- apps/reports/views.py
+++ apps/reports/views.py
@@ -11,36 +11,41 @@ import random
 #HOME VIEW#
 @login_required
 def reports_list(request): # /reports/
+    # declare variables
+    labels = []
+    data = []
+    color = []
+    # get all fields fron input form
     search_form = ChartSearchForm(request.POST or None)
     search=''
     if request.method == 'POST':
       search=request.POST['search']
-    
-    labels = []
-    data = []
-    
 
+    # get qs for chart - Wykres 1.
     qs = Item.objects.filter(itemOrder__isDone=False,itemMaterial__regex=search).annotate(
           result=F('itemDimmension1')/1000 * F('itemDimmension2')/1000 * F('itemQuantity'))
-
+    # pass qs to label and data for chart - Wykres 1
     for item in qs:
-        if item.result > 10:
-          labels.append(item.itemOrder.orderName+'-'+item.itemName+'-'+item.itemMaterial)
-          data.append(round(item.result,2))
-    
+        if item.result > 1:
+          labels.append(item.itemOrder.orderName+'-'+item.itemName+'-'+item.itemMaterial) #labels for chart
+          data.append(round(item.result,2)) #data for chart with rounded values
+          color.append('#%06x' % random.randint(0, 0xFFFFFF)) #random color for chart
+
+    # qs for counters
     def get_surface(boolean): #function to get surface of all items with a boolean value
+      surface_counter = 0 #counter for sum of surfaces
+      
       object_list = Item.objects.filter( itemOrder__isDone=boolean).annotate(
           result=F('itemDimmension1')/1000 * F('itemDimmension2')/1000 * F('itemQuantity')
         ) #calculate surface of all items with a boolean value - items in order done or not done
           #as filter pud underscores be used to access the values of the objects more on:
           #https://docs.djangoproject.com/en/4.0/topics/db/queries/ #
-      
-      surface_counter = 0 #counter for sum of surfaces
-      
+       
       for i in object_list: #loop to sum surfaces
         surface_counter+=i.result #add surface to counter
       return surface_counter #return sum of surfaces
 
+    print(color)
     context = {
       'title':'Raporty',
       'surface_counter_done':get_surface(True),
@@ -48,5 +53,6 @@ def reports_list(request): # /reports/
       'labels':labels,
       'data':data,
       'search_form':search_form,
+      'color':color,
       } #context for template
     return render (request,'reports/reports_list.html', context )
