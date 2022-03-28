f6c00c66ff5b367e249528eaff44399a3763676e
Jakub Klewicki <kuba.klewicki@gmail.com>
2022-03-28 15:44:20 +0200

    Update in buttons names & helper text Hide chart2 labels

--- apps/reports/forms.py
+++ apps/reports/forms.py
@@ -2,4 +2,4 @@ from email.policy import default
 from django import forms
 
 class ChartSearchForm(forms.Form):
-    search = forms.CharField(label='Wpisz szukaną frazę i naciśnij przycisk -> Szukaj:', max_length=100, required=False,)
+    search = forms.CharField(label='Wyszukaj po nazwie materiału, elementy do pokazania na wykresie:', max_length=100, required=False,)
