2bfd885feba77003b7a5fb746d75900397500941
TomPrint <tomekklewicki@gmail.com>
2022-03-30 21:29:04 +0200

    Corrected decorators and views formatting

--- apps/orders/views.py
+++ apps/orders/views.py
@@ -1,19 +1,26 @@
 from urllib import request
 from django.http import HttpRequest, HttpResponse
 from django.shortcuts import render
-from django.views.generic import CreateView, ListView, DeleteView, RedirectView, UpdateView
+from django.views.generic import (
+    CreateView,
+    ListView,
+    DeleteView,
+    RedirectView,
+    UpdateView,
+)
 from django_filters.views import FilterView
 from .filters import OrderFilter
 from apps import orders, users
 from .models import Order, Item
-from django.shortcuts import  get_object_or_404, redirect
+from django.shortcuts import get_object_or_404, redirect
 from django.contrib.auth.decorators import login_required
-from apps.users.decorators import admin_required, manager_required, operator_required
+from apps.users.decorators import admin_required, manager_required, operator_required, admin_or_manager_required
 from django.utils.decorators import method_decorator
 from django.contrib import messages
 from django.urls import reverse_lazy
 from .forms import OrderForm
 from django.core.mail import send_mail
+
 # IMPORTS FOR PDF #
 from datetime import datetime
 from xhtml2pdf import pisa
@@ -23,187 +30,230 @@ from django.template.loader import get_template
 
 # Create your views here.
 
-
-#passing request to form
+# passing request to form
 class PassRequestToFormViewMixin:
     def get_form_kwargs(self):
         kwargs = super().get_form_kwargs()
-        kwargs['request'] = self.request
+        kwargs["request"] = self.request
         return kwargs
+
+
 ###ORDER VIEWS####
 
 # CLASS - ORDER CREATE VIEW#
-@method_decorator(login_required, name='dispatch')
-@method_decorator(admin_required, name='dispatch')
-@method_decorator(manager_required, name='dispatch')
-class OrderCreateView (PassRequestToFormViewMixin, CreateView):
-  template_name = 'orders/orders_create.html'
-  form_class = OrderForm
-  
+@method_decorator(login_required, name="dispatch")
+@method_decorator(admin_or_manager_required, name="dispatch")
+class OrderCreateView(PassRequestToFormViewMixin, CreateView):
+    template_name = "orders/orders_create.html"
+    form_class = OrderForm
+
+
 # CLASS - ORDER LIST VIEW#
-@method_decorator(login_required, name='dispatch')
-class OrderListView (ListView):
-  model = Order # model to be used
-  template_name = 'orders/orders_list.html'
-  ordering = ['-orderDate']
+@method_decorator(login_required, name="dispatch")
+class OrderListView(ListView):
+    model = Order  # model to be used
+    template_name = "orders/orders_list.html"
+    ordering = ["-orderDate"]
 
-#CLASS - ORDER FINISH VIEW#
-@method_decorator(login_required, name='dispatch')
+
+# CLASS - ORDER FINISH VIEW#
+@method_decorator(login_required, name="dispatch")
 class OrderFinishView(RedirectView):
-  success_url = "/orders_list/" # Redirect user to the list of orders
+    success_url = "/orders_list/"  # Redirect user to the list of orders
 
-  def get(self, request, *args, **kwargs):
-      user_email = Order.objects.get(pk=kwargs['pk_order']).orderManager.email
-      order_name = Order.objects.get(pk=kwargs['pk_order']).orderName
-      order_number = Order.objects.get(pk=kwargs['pk_order']).orderNumber
-      order_id = self.kwargs['pk_order']  # pk_order is the name of the argument in the URL
-      order = get_object_or_404(Order, pk=order_id) # Get the order object
-      order.isDone = True # Set the order to done
-      order.save() # Save the order
-      messages.success(
-        request, 'POTWIERDZENIE - Zlecenie #{} {} zostało wykonane.'.format(order.orderNumber, order.orderName)
-        ) # show a success message
-      send_mail(f'Realizacja Zlecenia {order_number} w CutIt', f' Zlecenie o nazwie: "{order_name}" oraz numerze: "{order_number}", założone w CutIt przez {user_email}, zostało zakończone.','cutit.app.mail@gmail.com',
-                [f'{user_email}'], fail_silently=False)
-      return redirect(self.success_url) #Redirect to the list of orders
+    def get(self, request, *args, **kwargs):
+        user_email = Order.objects.get(pk=kwargs["pk_order"]).orderManager.email
+        order_name = Order.objects.get(pk=kwargs["pk_order"]).orderName
+        order_number = Order.objects.get(pk=kwargs["pk_order"]).orderNumber
+        order_id = self.kwargs[
+            "pk_order"
+        ]  # pk_order is the name of the argument in the URL
+        order = get_object_or_404(Order, pk=order_id)  # Get the order object
+        order.isDone = True  # Set the order to done
+        order.save()  # Save the order
+        messages.success(
+            request,
+            "POTWIERDZENIE - Zlecenie #{} {} zostało wykonane.".format(
+                order.orderNumber, order.orderName
+            ),
+        )  # show a success message
+        send_mail(
+            f"Realizacja Zlecenia {order_number} w CutIt",
+            f' Zlecenie o nazwie: "{order_name}" oraz numerze: "{order_number}", założone w CutIt przez {user_email}, zostało zakończone.',
+            "cutit.app.mail@gmail.com",
+            [f"{user_email}"],
+            fail_silently=False,
+        )
+        return redirect(self.success_url)  # Redirect to the list of orders
 
-#CLASS - ORDER RESTORE VIEW#
-@method_decorator(login_required, name='dispatch')
-@method_decorator(admin_required, name='dispatch')
-@method_decorator(manager_required, name='dispatch')
+
+# CLASS - ORDER RESTORE VIEW#
+@method_decorator(login_required, name="dispatch")
+@method_decorator(admin_or_manager_required, name="dispatch")
 class OrderRestoreView(RedirectView):
-  success_url = "/completed_orders_list/" # Redirect user to the list of completed orders
+    success_url = (
+        "/completed_orders_list/"  # Redirect user to the list of completed orders
+    )
 
-  def get(self, request, *args, **kwargs):
-      order_id = self.kwargs['pk_order']  # pk_order is the name of the argument in the URL
-      order = get_object_or_404(Order, pk=order_id) # Get the order object
-      # order.pk = None # Set the order to done - will clone order
-      order.isDone = False # Set the order to not done
-      order.save() # Save the order
-      messages.warning(
-        request,
-        f'UWAGA - Zlecenie # {order.orderNumber} {order.orderName} zostało przywrócone do planu cięcia.'
-        ) # show a warning message using f-string
-      return redirect(self.success_url) #Redirect to the list of completed orders
+    def get(self, request, *args, **kwargs):
+        order_id = self.kwargs[
+            "pk_order"
+        ]  # pk_order is the name of the argument in the URL
+        order = get_object_or_404(Order, pk=order_id)  # Get the order object
+        # order.pk = None # Set the order to done - will clone order
+        order.isDone = False  # Set the order to not done
+        order.save()  # Save the order
+        messages.warning(
+            request,
+            f"UWAGA - Zlecenie # {order.orderNumber} {order.orderName} zostało przywrócone do planu cięcia.",
+        )  # show a warning message using f-string
+        return redirect(self.success_url)  # Redirect to the list of completed orders
 
-#CLASS - ORDER DELETE VIEW
-@method_decorator(login_required, name='dispatch')
-@method_decorator(admin_required, name='dispatch')
-@method_decorator(manager_required, name='dispatch')
+
+# CLASS - ORDER DELETE VIEW
+@method_decorator(login_required, name="dispatch")
+@method_decorator(admin_or_manager_required, name="dispatch")
 class OrderDeleteView(DeleteView):
-  model=Order
-  template_name = 'orders/orders_delete.html'
-  success_url = '/orders_list/'
+    model = Order
+    template_name = "orders/orders_delete.html"
+    success_url = "/orders_list/"
 
 
-#COMPLETED ORDERS VIEW#
-@method_decorator(login_required, name='dispatch')
-class OrderCompletedListView (FilterView):
-  model = Order # model to be used
-  template_name = 'orders/completed_orders_list.html'
-  filterset_class = OrderFilter
-  # query_set for dajango-filter
-  def get_queryset(self):
-    qs = self.model.objects.filter(isDone=True)
-    search = OrderFilter(self.request.GET, queryset=qs)
-    return search.qs
+# COMPLETED ORDERS VIEW#
+@method_decorator(login_required, name="dispatch")
+class OrderCompletedListView(FilterView):
+    model = Order  # model to be used
+    template_name = "orders/completed_orders_list.html"
+    filterset_class = OrderFilter
+    # query_set for dajango-filter
+    def get_queryset(self):
+        qs = self.model.objects.filter(isDone=True)
+        search = OrderFilter(self.request.GET, queryset=qs)
+        return search.qs
 
-#ORDER UPDATE VIEW#
-@method_decorator(login_required, name='dispatch')
-@method_decorator(admin_required, name='dispatch')
-@method_decorator(manager_required, name='dispatch')
+
+# ORDER UPDATE VIEW#
+@method_decorator(login_required, name="dispatch")
+@method_decorator(admin_or_manager_required, name="dispatch")
 class OrderUpdateView(UpdateView):
-  model = Order
-  fields = ['orderNumber','orderName', 'orderQuantity','orderNotes']
-  template_name = 'orders/orders_create.html'
+    model = Order
+    fields = ["orderNumber", "orderName", "orderQuantity", "orderNotes"]
+    template_name = "orders/orders_create.html"
+
 
 ###ITEM VIEWS####
 
-#CLASS - ITEM LIST VIEW
-@method_decorator(login_required, name='dispatch')
-class ItemListView (ListView):
-  model = Item # model to be used
-  template_name = 'orders/items_list.html' # template to be used
+# CLASS - ITEM LIST VIEW
+@method_decorator(login_required, name="dispatch")
+class ItemListView(ListView):
+    model = Item  # model to be used
+    template_name = "orders/items_list.html"  # template to be used
 
-  def get_context_data(self, **kwargs):
-    context = super().get_context_data(**kwargs) # Get the context from the super class
-    context['order'] = get_object_or_404(Order, pk=self.kwargs['pk_order']) # Get the order object
-    return context # Return the context
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(
+            **kwargs
+        )  # Get the context from the super class
+        context["order"] = get_object_or_404(
+            Order, pk=self.kwargs["pk_order"]
+        )  # Get the order object
+        return context  # Return the context
 
-#CLASS - ITEM CREATE VIEW
-@method_decorator(login_required, name='dispatch')
-@method_decorator(admin_required, name='dispatch')
-@method_decorator(manager_required, name='dispatch')
-class ItemCreateView (CreateView):
-  model=Item
-  template_name = 'orders/items_create.html'
-  fields = ['itemMaterial', 'itemName', 'itemQuantity', 'itemDimmension1', 'itemBander', 'itemDimmension2', 'itemBander2']
-  
-  def form_valid(self, form):
-    form.instance.itemOrder = get_object_or_404(Order, pk=self.kwargs['pk_order']) # Get the order object
-    return super(ItemCreateView, self).form_valid(form) # Return the form valid
 
-#CLASS - ITEM DELETE VIEW
-@method_decorator(login_required, name='dispatch')
-@method_decorator(admin_required, name='dispatch')
-@method_decorator(manager_required, name='dispatch')
+# CLASS - ITEM CREATE VIEW
+@method_decorator(login_required, name="dispatch")
+@method_decorator(admin_or_manager_required, name="dispatch")
+class ItemCreateView(CreateView):
+    model = Item
+    template_name = "orders/items_create.html"
+    fields = [
+        "itemMaterial",
+        "itemName",
+        "itemQuantity",
+        "itemDimmension1",
+        "itemBander",
+        "itemDimmension2",
+        "itemBander2",
+    ]
+
+    def form_valid(self, form):
+        form.instance.itemOrder = get_object_or_404(
+            Order, pk=self.kwargs["pk_order"]
+        )  # Get the order object
+        return super(ItemCreateView, self).form_valid(form)  # Return the form valid
+
+
+# CLASS - ITEM DELETE VIEW
+@method_decorator(login_required, name="dispatch")
+@method_decorator(admin_or_manager_required, name="dispatch")
 class ItemDeleteView(DeleteView):
-  model=Item
-  template_name = 'orders/items_delete.html'
-  
-  def get_success_url(self): # Redirect user to the list of items
-    item = get_object_or_404(Item, pk=self.kwargs['pk']) # Get the item object for message purpose
-    messages.info (
-      self.request, 
-      f'USNIĘTO POZYCJĘ CIĘCIA - {item.itemName} o wymiarach {item.itemDimmension1} x {item.itemDimmension2} mm'
-    ) # show a success message
-    return reverse_lazy('page-items-list', kwargs={'pk_order': self.kwargs['pk_order']})
+    model = Item
+    template_name = "orders/items_delete.html"
 
-#CLASS - ITEM UPDATE VIEW
-@method_decorator(admin_required, name='dispatch')
-@method_decorator(manager_required, name='dispatch')
+    def get_success_url(self):  # Redirect user to the list of items
+        item = get_object_or_404(
+            Item, pk=self.kwargs["pk"]
+        )  # Get the item object for message purpose
+        messages.info(
+            self.request,
+            f"USNIĘTO POZYCJĘ CIĘCIA - {item.itemName} o wymiarach {item.itemDimmension1} x {item.itemDimmension2} mm",
+        )  # show a success message
+        return reverse_lazy(
+            "page-items-list", kwargs={"pk_order": self.kwargs["pk_order"]}
+        )
+
+
+# CLASS - ITEM UPDATE VIEW
+@method_decorator(login_required, name="dispatch")
+@method_decorator(admin_or_manager_required, name="dispatch")
 class ItemUpdateView(UpdateView):
-  model=Item
-  template_name = 'orders/items_create.html'
-  fields = ['itemMaterial', 'itemName', 'itemQuantity', 'itemDimmension1', 'itemBander', 'itemDimmension2', 'itemBander2']
+    model = Item
+    template_name = "orders/items_create.html"
+    fields = [
+        "itemMaterial",
+        "itemName",
+        "itemQuantity",
+        "itemDimmension1",
+        "itemBander",
+        "itemDimmension2",
+        "itemBander2",
+    ]
+
 
 ### ADITIONAL FUNCTIONALITY ####
 
 # PDF VIEWS#
 @login_required
-@admin_required
-@manager_required
+@admin_or_manager_required
 def render_pdf_view(request, pk):
-    template_path = 'orders/pdf.html'
+    template_path = "orders/pdf.html"
     obj = get_object_or_404(Order, pk=pk)
-    obj2= Item.objects.filter(itemOrder=obj)
+    obj2 = Item.objects.filter(itemOrder=obj)
     obj3 = datetime.now()
 
-    context = {
-      'obj': obj,
-      'obj2': obj2,
-      'obj3': obj3
-    }
+    context = {"obj": obj, "obj2": obj2, "obj3": obj3}
 
-    response = HttpResponse(content_type='orders/pdf')
-    response['Content-Disposition'] = 'filename="Zlecenie {} w PDF.pdf"'.format(obj.orderNumber)
+    response = HttpResponse(content_type="orders/pdf")
+    response["Content-Disposition"] = 'filename="Zlecenie {} w PDF.pdf"'.format(
+        obj.orderNumber
+    )
     template = get_template(template_path)
     html = template.render(context)
-    
-    pisa_status = pisa.CreatePDF(
-        html, dest=response)
+
+    pisa_status = pisa.CreatePDF(html, dest=response)
 
     if pisa_status.err:
-        return HttpResponse('We had some errors <pre>' + html + '</pre>')
+        return HttpResponse("We had some errors <pre>" + html + "</pre>")
     return response
 
-#HOME VIEW#
+
+# HOME VIEW#
 @login_required
 def home(request):
-    
-    context = {
-        'title':'Strona startowa programu',
-        'orders': Order.objects.all().filter(orderManager=request.user).order_by('-orderDate')[:15],
-      }
-    return render (request,'orders/home.html', context )
 
+    context = {
+        "title": "Strona startowa programu",
+        "orders": Order.objects.all()
+        .filter(orderManager=request.user)
+        .order_by("-orderDate")[:15],
+    }
+    return render(request, "orders/home.html", context)
--- apps/reports/views.py
+++ apps/reports/views.py
@@ -10,75 +10,92 @@ import random
 from django.db.models import Count
 
 
-#HOME VIEW#
+# HOME VIEW#
 @login_required
-def reports_list(request): # /reports/
+def reports_list(request):  # /reports/
     # declare variables
     labels = []
-    labels2=[]
+    labels2 = []
     data = []
     data2 = []
     color = []
-    counter_list=[]
-    search=''
+    counter_list = []
+    search = ""
     counter = 0
     # get search value fron input form
     search_form = ChartSearchForm(request.POST or None)
-    if request.method == 'POST':
-      search=request.POST['search']
+    if request.method == "POST":
+        search = request.POST["search"]
 
     # get qs for chart - Wykres 1.
-    qs = Item.objects.filter(itemOrder__isDone=False,itemMaterial__regex=search).annotate(
-          result=F('itemDimmension1')/1000 * F('itemDimmension2')/1000 * F('itemQuantity'))
+    qs = Item.objects.filter(
+        itemOrder__isDone=False, itemMaterial__regex=search
+    ).annotate(
+        result=F("itemDimmension1")
+        / 1000
+        * F("itemDimmension2")
+        / 1000
+        * F("itemQuantity")
+    )
     # pass qs to label and data for chart - Wykres 1
     for item in qs:
         if item.result > 0:
-          labels.append(item.itemOrder.orderName+'-'+item.itemName+'-'+item.itemMaterial) #labels for chart
-          data.append(round(item.result,2)) #data for chart with rounded values
-          color.append('#%06x' % random.randint(0, 0xFFFFFF)) #random color for chart
-    
-    # get qs for chart - Wykres 2. 
+            labels.append(
+                item.itemOrder.orderName + "-" + item.itemName + "-" + item.itemMaterial
+            )  # labels for chart
+            data.append(round(item.result, 2))  # data for chart with rounded values
+            color.append(
+                "#%06x" % random.randint(0, 0xFFFFFF)
+            )  # random color for chart
+
+    # get qs for chart - Wykres 2.
     qs2 = Order.objects.all()
     qs3 = User.objects.all()
 
     # prepare labels list for chart - Wykres 2
     for user in qs3:
-      labels2.append(str(user.username))
-    
+        labels2.append(str(user.username))
+
     # prepare data list for chart - Wykres 2
     for user in labels2:
-      for order in qs2:
-        if user == order.orderManager.username: #check if user is manager of order
-          counter+=1
-      counter_list.append(counter) # count for first user then second user etc.
-      counter=0 # reset counter for next user
-    
-    #finally data for chart - Wykres 2
+        for order in qs2:
+            if user == order.orderManager.username:  # check if user is manager of order
+                counter += 1
+        counter_list.append(counter)  # count for first user then second user etc.
+        counter = 0  # reset counter for next user
+
+    # finally data for chart - Wykres 2
     data2 = counter_list
 
     # qs for statistic counters
-    def get_surface(boolean): #function to get surface of all items with a boolean value
-      surface_counter = 0 #counter for sum of surfaces
-      
-      object_list = Item.objects.filter( itemOrder__isDone=boolean).annotate(
-          result=F('itemDimmension1')/1000 * F('itemDimmension2')/1000 * F('itemQuantity')
-        ) #calculate surface of all items with a boolean value - items in order done or not done
-          #as filter pud underscores be used to access the values of the objects more on:
-          #https://docs.djangoproject.com/en/4.0/topics/db/queries/ #
-       
-      for i in object_list: #loop to sum surfaces
-        surface_counter+=i.result #add surface to counter
-      return surface_counter #return sum of surfaces
-    
+    def get_surface(
+        boolean,
+    ):  # function to get surface of all items with a boolean value
+        surface_counter = 0  # counter for sum of surfaces
+
+        object_list = Item.objects.filter(itemOrder__isDone=boolean).annotate(
+            result=F("itemDimmension1")
+            / 1000
+            * F("itemDimmension2")
+            / 1000
+            * F("itemQuantity")
+        )  # calculate surface of all items with a boolean value - items in order done or not done
+        # as filter pud underscores be used to access the values of the objects more on:
+        # https://docs.djangoproject.com/en/4.0/topics/db/queries/ #
+
+        for i in object_list:  # loop to sum surfaces
+            surface_counter += i.result  # add surface to counter
+        return surface_counter  # return sum of surfaces
+
     context = {
-      'title':'Raporty',
-      'surface_counter_done':get_surface(True),
-      'surface_counter_not_done':get_surface(False),
-      'labels':labels,
-      'labels2':labels2,
-      'data':data,
-      'data2':data2,
-      'search_form':search_form,
-      'color':color,
-      } #context for template
-    return render (request,'reports/reports_list.html', context )
+        "title": "Raporty",
+        "surface_counter_done": get_surface(True),
+        "surface_counter_not_done": get_surface(False),
+        "labels": labels,
+        "labels2": labels2,
+        "data": data,
+        "data2": data2,
+        "search_form": search_form,
+        "color": color,
+    }  # context for template
+    return render(request, "reports/reports_list.html", context)
