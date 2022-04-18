aecf82ebadfe2a50282d7d58270f855fc7679bd4
Jakub Klewicki <kuba.klewicki@gmail.com>
2022-04-18 11:12:40 +0200

    Update export xls - change headers name for PL

--- apps/orders/admin.py
+++ apps/orders/admin.py
@@ -10,13 +10,27 @@ class ItemResource(resources.ModelResource):
     itemOrder=Field()
     class Meta:
         model = Item
-        fields = ('itemMaterial', 'itemDimmension1', 'itemDimmension2', 'itemQuantity', 'itemName','itemOrder','isDone')
+        fields = ('itemMaterial', 'itemDimmension1','itemBander', 'itemDimmension2','itemBander2', 'itemQuantity', 'itemName','itemOrder','isDone')
         export_order = fields
 
     def dehydrate_isDone(self, obj):
         return str(obj.isDone)
     def dehydrate_itemOrder(self, obj):
         return str(obj.itemOrder.orderNumber) + "-" + str(obj.itemOrder.orderName)
+
+    def get_export_headers(self):
+        headers = super().get_export_headers()
+        for i, h in enumerate(headers):
+            if h == 'isDone': headers[i] = "Zrealizowano?"
+            if h == 'itemDimmension1': headers[i] = "Wymiar 1"
+            if h == 'itemDimmension2': headers[i] = "Wymiar 2"
+            if h == 'itemQuantity': headers[i] = "Ilość"
+            if h == 'itemName': headers[i] = "Nazwa części"
+            if h == 'itemOrder': headers[i] = "Zlecenie"
+            if h == 'itemMaterial': headers[i] = "Materiał"
+            if h == 'itemBander': headers[i] = "Oklejanie?"
+            if h == 'itemBander2': headers[i] = "Oklejanie?"
+        return headers
 
 class ItemAdmin(ExportActionMixin, admin.ModelAdmin):
     resource_class = ItemResource
