0cfba9dc2f4bbd90ae80c7d217cb75e44c069ff5
TomPrint <tomekklewicki@gmail.com>
2022-06-20 22:15:06 +0200

    headless chrome configuration

--- cutIt_app/settings.py
+++ cutIt_app/settings.py
@@ -159,6 +159,7 @@ EMAIL_HOST_PASSWORD = os.environ.get("EMAIL_KEY_CUTIT")
 # for development your local path to chrome.exe
 CHROME_PATH = '"C:\Program Files\Google\Chrome\Application\chrome.exe"'
 CHROME_WINDOW_SIZE = "800,600"
+CHROME_PATH = "google-chrome"
 
 AWS_ACCESS_KEY_ID = os.environ.get("AWS_CUTIT_ID")
 AWS_SECRET_ACCESS_KEY = os.environ.get("AWS_CUTIT_KEY")
