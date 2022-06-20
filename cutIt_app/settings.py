d2ee35d6c83825a27f70a7602f9ce14b8512264b
TomPrint <tomekklewicki@gmail.com>
2022-06-21 01:59:13 +0200

    headless chrome update

--- cutIt_app/settings.py
+++ cutIt_app/settings.py
@@ -26,8 +26,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get("SECRET_KEY_CUTIT")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = (os.environ.get('DEBUG_VALUE') == 'True')
-
+# DEBUG = (os.environ.get('DEBUG_VALUE') == 'True')
+DEBUG = True
 ALLOWED_HOSTS = ['cutit-ats.herokuapp.com', ".herokuapp.com"]
 
 
