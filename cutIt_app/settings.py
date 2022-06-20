2e7b308d1d0258592a17c53c20bdf1c61123153f
TomPrint <tomekklewicki@gmail.com>
2022-06-21 00:50:07 +0200

    headless chrome update

--- cutIt_app/settings.py
+++ cutIt_app/settings.py
@@ -162,15 +162,15 @@ EMAIL_HOST_PASSWORD = os.environ.get("EMAIL_KEY_CUTIT")
 from selenium import webdriver
 import os
 
-CHROMEDRIVER_PATH = '/app/.chromedriver/bin/chromedriver'
-GOOGLE_CHROME_BIN = '/app/.apt/usr/bin/google-chrome' 
+CHROMEDRIVER_PATH = /app/.chromedriver/bin/chromedriver
+GOOGLE_CHROME_BIN = /app/.apt/usr/bin/google-chrome
 
 chrome_options = webdriver.ChromeOptions()
-chrome_options.binary_location = os.environ.get(GOOGLE_CHROME_BIN)
+chrome_options.binary_location = os.environ.get('GOOGLE_CHROME_BIN')
 chrome_options.add_argument("--headless")
 chrome_options.add_argument("--disable-dev-shm-usage")
 chrome_options.add_argument("--no-sandbox")
-driver = webdriver.Chrome(executable_path=os.environ.get(CHROMEDRIVER_PATH), chrome_options=chrome_options)
+driver = webdriver.Chrome(executable_path=os.environ.get('CHROMEDRIVER_PATH'), chrome_options=chrome_options)
 
 
 #hardcopy settings
