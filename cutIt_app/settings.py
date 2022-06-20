41d8485b65fdf88b319336e78c79ce4bbb9a5926
TomPrint <tomekklewicki@gmail.com>
2022-06-21 01:19:10 +0200

    headless chrome update

--- cutIt_app/settings.py
+++ cutIt_app/settings.py
@@ -157,21 +157,6 @@ EMAIL_HOST_USER = "cutit.app.mail@gmail.com"
 EMAIL_HOST_PASSWORD = os.environ.get("EMAIL_KEY_CUTIT")
 
 
-#pdf headless chrome setup for development
-
-from selenium import webdriver
-import os
-
-CHROMEDRIVER_PATH = '/app/.chromedriver/bin/chromedriver'
-GOOGLE_CHROME_BIN = '/app/.apt/usr/bin/google-chrome'
-
-chrome_options = webdriver.ChromeOptions()
-chrome_options.binary_location = os.environ.get(GOOGLE_CHROME_BIN)
-chrome_options.add_argument("--headless")
-chrome_options.add_argument("--disable-dev-shm-usage")
-chrome_options.add_argument("--no-sandbox")
-driver = webdriver.Chrome(executable_path=os.environ.get(CHROMEDRIVER_PATH), chrome_options=chrome_options)
-
 
 #hardcopy settings
 
@@ -179,7 +164,7 @@ driver = webdriver.Chrome(executable_path=os.environ.get(CHROMEDRIVER_PATH), chr
 # CHROME_PATH = '"C:\Program Files\Google\Chrome\Application\chrome.exe"'
 CHROME_WINDOW_SIZE = "800,600"
 # CHROME_PATH = "google-chrome"
-CHROME_PATH = driver
+CHROME_PATH = "/app/.apt/usr/bin/google-chrome"
 
 
 AWS_ACCESS_KEY_ID = os.environ.get("AWS_CUTIT_ID")
