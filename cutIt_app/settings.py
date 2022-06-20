af7b512ce8afce7bac4518e52a90327b40f03402
TomPrint <tomekklewicki@gmail.com>
2022-06-21 01:50:11 +0200

    headless chrome update

--- cutIt_app/settings.py
+++ cutIt_app/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = os.environ.get("SECRET_KEY_CUTIT")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = (os.environ.get('DEBUG_VALUE') == 'True')
 
-ALLOWED_HOSTS = ['cutit-ats.herokuapp.com']
+ALLOWED_HOSTS = ['cutit-ats.herokuapp.com', ".herokuapp.com"]
 
 
 # Application definition
