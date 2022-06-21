4d23b176242575480ca0c86aa854fdcc328a0ac3
TomPrint <tomekklewicki@gmail.com>
2022-06-21 23:21:30 +0200

    update

--- cutIt_app/settings.py
+++ cutIt_app/settings.py
@@ -171,7 +171,7 @@ EMAIL_USE_TLS = True
 EMAIL_HOST_USER = "cutit.app.mail@gmail.com"
 EMAIL_HOST_PASSWORD = os.environ.get("EMAIL_KEY_CUTIT")
 
-
+'DIRS': [os.path.join(BASE_DIR, 'templates')]
 
 
 # hardcopy settings
