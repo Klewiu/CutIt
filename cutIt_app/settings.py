e70a878dc20612d6987dbacd2a8d413455eae425
TomPrint <tomekklewicki@gmail.com>
2022-06-21 23:49:03 +0200

    updated

--- cutIt_app/settings.py
+++ cutIt_app/settings.py
@@ -131,7 +131,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
-STATIC_URL = "/static/"
+STATIC_URL = "static/"
 STATICFILES_DIRS = (os.path.join(BASE_DIR, "static"),)
 
 # STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
@@ -141,7 +141,7 @@ STATICFILES_DIRS = (os.path.join(BASE_DIR, "static"),)
 # )
 
 # media files
-MEDIA_URL = "/media/"
+MEDIA_URL = "media/"
 MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 
 WHITENOISE_USE_FINDERS = True
