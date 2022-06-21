d5b955d4bd0eb7f7da4d4fffa02d3bc194f51e0c
TomPrint <tomekklewicki@gmail.com>
2022-06-21 22:57:19 +0200

    update

--- cutIt_app/settings.py
+++ cutIt_app/settings.py
@@ -61,6 +61,7 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = "cutIt_app.urls"
@@ -142,6 +143,8 @@ STATICFILES_DIRS = (
 # media files
 MEDIA_URL = "/media/"
 MEDIA_ROOT = os.path.join(BASE_DIR, "media")
+
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 
 # STATIC_URL = '/static/'
