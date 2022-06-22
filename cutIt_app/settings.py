1c11cbc790776c11f3deedbda53c228a4c0bad5d
TomPrint <tomekklewicki@gmail.com>
2022-06-22 21:52:04 +0200

    added logging for error trace

--- cutIt_app/settings.py
+++ cutIt_app/settings.py
@@ -12,7 +12,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 
 from pathlib import Path
 import os
-import django_heroku
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -206,7 +206,5 @@ LOGGING = {
             'propagate': False,
         },
     }
+}
 
-
-
-django_heroku.settings(locals())
