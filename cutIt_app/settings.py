ff69a5132fca270e78554f82d15df676f0a9f981
TomPrint <tomekklewicki@gmail.com>
2022-06-22 23:01:48 +0200

    updated heroku

--- cutIt_app/settings.py
+++ cutIt_app/settings.py
@@ -88,7 +88,7 @@ WSGI_APPLICATION = "cutIt_app.wsgi.application"
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
 
-DATABASES['default'] = dj_database_url.config(conn_max_age=600, ssl_require=True)
+
 
 DATABASES = {
     "default": {
@@ -97,6 +97,7 @@ DATABASES = {
     }
 }
 
+DATABASES['default'] = dj_database_url.config(conn_max_age=600, ssl_require=True)
 
 # Password validation
 # https://docs.djangoproject.com/en/4.0/ref/settings/#auth-password-validators
